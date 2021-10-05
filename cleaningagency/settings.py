6c1d005b9b64a341b80934d224c4acdae764b676
bensammwaniki <bensammwaniki@gmail.com>
2021-10-05 23:47:29 +0300

    adding postgresql

--- cleaningagency/settings.py
+++ cleaningagency/settings.py
@@ -72,14 +72,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cleaningagency.wsgi.application'
 
+# TIME_ZONE
+
+TIME_ZONE = 'Africa/Nairobi'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polished',
+        'USER': 'moringa',
+    'PASSWORD':'Access',
     }
 }
 
