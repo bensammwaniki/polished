248414970c9859e9cc9197aad0cfb93519e90d7b
bensammwaniki <bensammwaniki@gmail.com>
2021-10-06 22:42:32 +0300

    new booking migrations

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Bookings(models.Model):
+    name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_no = models.IntegerField()
+    postal_address = models.CharField(max_length =30)
+    city = models.CharField(max_length =30, default='Nairobi', editable=False)
+    servicies = models.CharField(max_length =30)
+    comments = models.CharField(max_length =500)
+
+
+
+
